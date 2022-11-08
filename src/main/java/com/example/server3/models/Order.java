70668235daf96595279008c60e58c3d49437cbda
Dina <dina.bitca@isa.utm.md>
2022-11-08 10:19:49 +0200

    Simplifying Order Object

--- src/main/java/com/example/server3/models/Order.java
+++ src/main/java/com/example/server3/models/Order.java
@@ -3,7 +3,6 @@ package com.example.server3.models;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.ArrayList;
 
 public class Order {
     private int id;
